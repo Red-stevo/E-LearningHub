0e87b150e9d13664efb90b9cc2812913f630c6cc
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-21 12:47:27 +0300

    coonfigured the jwt filter done

--- StudentLearningAssistant/src/main/java/com/redstevo/code/Filters/JwtFilter.java
+++ StudentLearningAssistant/src/main/java/com/redstevo/code/Filters/JwtFilter.java
@@ -9,9 +9,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -57,8 +59,17 @@ public class JwtFilter extends OncePerRequestFilter {
 
             if(jwtService.isValid(userDetails, jwt)){
 
-            }
+                UsernamePasswordAuthenticationToken token =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails,
+                                null,
+                                userDetails.getAuthorities());
 
+                token.setDetails(
+                        new WebAuthenticationDetailsSource()
+                );
+                SecurityContextHolder.getContext().setAuthentication(token);
+            }
         }
         filterChain.doFilter(request,response);
     }
