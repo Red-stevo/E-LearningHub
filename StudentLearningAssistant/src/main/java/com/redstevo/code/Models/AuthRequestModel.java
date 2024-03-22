b6a17d934777b860a2f657cf8a71d2cf644c4d86
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-22 11:19:38 +0300

    check the password parttern

--- StudentLearningAssistant/src/main/java/com/redstevo/code/Models/AuthRequestModel.java
+++ StudentLearningAssistant/src/main/java/com/redstevo/code/Models/AuthRequestModel.java
@@ -1,5 +1,8 @@
 package com.redstevo.code.Models;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
@@ -7,9 +10,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class AuthRequestModel {
 
+    @NotNull
     private String username;
 
+    @Pattern(regexp = "^?=.*\\p{Upper}?=.*\\p{Lower}?=.*\\p{N}?=.*\\p{S}$" ,
+    message = "The password must contain at least 1 Upper case character, at least ! lowercase character, a number " +
+            "and a special character")
+    @Size(min = 8, max = 20, message = "Password must contain 8 - 20 Alphanumerics")
     private  String password;
 
+    @NotNull
     private String Email;
 }
