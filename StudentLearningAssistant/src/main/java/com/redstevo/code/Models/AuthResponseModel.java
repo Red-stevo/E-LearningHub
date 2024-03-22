6f779890ee790deabeb97b533b59599f13316d36
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-22 05:45:34 +0300

    design the response model

--- StudentLearningAssistant/src/main/java/com/redstevo/code/Models/AuthResponseModel.java
+++ StudentLearningAssistant/src/main/java/com/redstevo/code/Models/AuthResponseModel.java
@@ -1,8 +1,48 @@
 package com.redstevo.code.Models;
 
+import lombok.Getter;
 import org.springframework.stereotype.Component;
 
+@Getter
 @Component
 public class AuthResponseModel {
 
+    private String username;
+
+    private String message;
+
+    private String email;
+
+    private String jwt;
+
+    private Long id;
+
+    public AuthResponseModel setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public AuthResponseModel setMessage(String message) {
+        this.message = message;
+        return this;
+    }
+
+    public AuthResponseModel setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public AuthResponseModel setJwt(String jwt) {
+        this.jwt = jwt;
+        return this;
+    }
+
+    public AuthResponseModel setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public void build(){
+
+    }
 }
