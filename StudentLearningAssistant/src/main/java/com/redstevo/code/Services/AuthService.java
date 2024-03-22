dc06c00e4c5c882219691d5a7a584fab3371de5e
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-22 07:20:30 +0300

    taking break

--- StudentLearningAssistant/src/main/java/com/redstevo/code/Services/AuthService.java
+++ StudentLearningAssistant/src/main/java/com/redstevo/code/Services/AuthService.java
@@ -4,8 +4,10 @@ import com.redstevo.code.Models.AuthRequestModel;
 import com.redstevo.code.Models.AuthResponseModel;
 import com.redstevo.code.Repositories.AuthRepository;
 import com.redstevo.code.Repositories.ProfileRepository;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -30,5 +32,10 @@ public class AuthService {
 
 
         return null;
+    }
+
+    public ResponseEntity<Boolean> isUsernameAvailable(
+            @Size(min = 2, max = 20, message = "Username Must Be Between 2-20 characters.") String username) {
+        return new ResponseEntity<>((authRepository.countAllByUsername(username).orElse(0) == 0), HttpStatus.OK);
     }
 }
