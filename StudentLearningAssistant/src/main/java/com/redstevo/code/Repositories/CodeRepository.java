eff9b0c2a8b36f020027f79c6f17cea122884a17
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-24 14:47:52 +0300

    added the code repo

---
+++ StudentLearningAssistant/src/main/java/com/redstevo/code/Repositories/CodeRepository.java
@@ -0,0 +1,11 @@
+package com.redstevo.code.Repositories;
+
+import com.redstevo.code.Tables.Code;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CodeRepository extends JpaRepository<Code, String> {
+
+    Optional<Code> findByUsername(String username);
+}
