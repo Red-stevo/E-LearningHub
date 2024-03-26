399870c219e1790e527e8761b9d0a5f3ed508fde
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-03-26 06:37:14 +0300

    handled validation exception

--- StudentLearningAssistant/src/main/java/com/redstevo/code/Models/AuthRequestModel.java
+++ StudentLearningAssistant/src/main/java/com/redstevo/code/Models/AuthRequestModel.java
@@ -13,10 +13,10 @@ public class AuthRequestModel {
     @Size(max = 50, min = 2, message = "Username Must Be Between 2 - 50 characters.")
     private String username;
 
-    @Pattern(regexp = "^(?=.*\\p{Upper})(?=.*\\p{Lower})(?=.*\\d)(?=.*\\p{S}).*$",
-    message = "The password must contain at least 1 Upper case character, at least ! lowercase character, a number " +
-            "and a special character")
-    @Size(min = 8, max = 20, message = "Password must contain 8 - 20 Alphanumerics")
+
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^!`~*/.,<>?_;:&-+=()])(?=\\S+$).{8,20}$",
+    message = "The password must contain at least 1 Upper case character, at least 1 lowercase character, a number " +
+            "a special character and should have 8 - 20 characters")
     private  String password;
 
     @Email(message = "You Entered An Invalid Email.")
