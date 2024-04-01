23737771c329f6b902c5a72c184179af8309a00e
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-01 14:47:22 +0300

    done

--- StudentLearningAssistant/src/main/java/com/redstevo/code/Configurations/SecurityConfig.java
+++ StudentLearningAssistant/src/main/java/com/redstevo/code/Configurations/SecurityConfig.java
@@ -17,7 +17,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Component;
 
 @Slf4j
