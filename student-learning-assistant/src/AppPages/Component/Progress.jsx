b3d9f48204105372c4d449d800791b187fa3dc07
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-09 13:54:16 +0300

    moving forward to registration form design

--- student-learning-assistant/src/AppPages/Component/Progress.jsx
+++ student-learning-assistant/src/AppPages/Component/Progress.jsx
@@ -4,13 +4,19 @@ import './../Styles/RegistrationPage.css'
 const Progress = ( { register, verify, profile } ) => {
     return(
         <div className={"bar"}>
-            <ProgressBar >
-                <ProgressBar striped={false} now={register} className={'register'} variant={'danger'}/>
-                <ProgressBar striped={false} now={verify} className={'verify-email'} variant={'success'}/>
-                <ProgressBar striped={false} now={profile} className={'profile'} variant={'info'}/>
+            <ProgressBar className={'bg-dark'} >
+                <ProgressBar
+                    striped={false} now={register} className={'register'} variant={'danger'}
+                    label={"register"}/>
+                <ProgressBar
+                    striped={false} now={verify} className={'verify-email'} variant={'success'}
+                    label={"verify email"}/>
+                <ProgressBar
+                    striped={false} now={profile} className={'profile'} variant={'info'}
+                    label={"(Optional)user profile"}/>
             </ProgressBar>
         </div>
-    )
+    );
 }
 
 export default Progress;
--- student-learning-assistant/src/AppPages/RoutedPages/RegistrationPage.jsx
+++ student-learning-assistant/src/AppPages/RoutedPages/RegistrationPage.jsx
@@ -5,12 +5,17 @@ import Progress from "../Component/Progress.jsx";
 
 export const RegistrationPage = () => {
     const [register, setRegister] = useState(40);
-    const [verify, setVerify] = useState(40);
-    const [profile, setProfile] = useState(20)
+    const [verify, setVerify] = useState(0);
+    const [profile, setProfile] = useState(0);
+    const [registrationPage, setRegistrationPage] = useState(false);
 
     return(
         <Container fluid={"xxl"}>
             <Progress register={register} verify={verify} profile={profile}/>
+
+            <div>
+                {registrationPage && <></>}
+            </div>
         </Container>
     );
 }
