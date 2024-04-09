c1582d65c90f4c51c548b89b8d5b0d2c394075bb
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-09 14:28:39 +0300

    registration form design done

--- student-learning-assistant/src/AppPages/Component/RegistrationForm.jsx
+++ student-learning-assistant/src/AppPages/Component/RegistrationForm.jsx
@@ -4,22 +4,22 @@ const RegistrationForm = () => {
     return(
         <div className={'reg-form'}>
             <Form>
-                <FormLabel>REGISTER</FormLabel>
-                <Form.Group>
+                <legend><FormLabel>REGISTER</FormLabel></legend>
+                <Form.Group className={'username'}>
                     <Form.Label>USERNAME</Form.Label>
                     <Form.Control type={'text'} placeholder={'mike-meta'}/>
                 </Form.Group>
-                <Form.Group>
+                <Form.Group className={'password'}>
                     <Form.Label>PASSWORD</Form.Label>
-                    <Form.Control type={'text'} placeholder={'CREATE A PASSWORD'}/>
+                    <Form.Control type={'text'} placeholder={'create a password'}/>
                 </Form.Group>
-                <Form.Group>
+                <Form.Group className={'confirm'}>
                     <Form.Label>CONFIRM PASSWORD</Form.Label>
-                    <Form.Control type={'text'} placeholder={'mike-meta'}/>
+                    <Form.Control type={'text'} placeholder={'re-enter the password'}/>
                 </Form.Group>
-                <Form.Group>
+                <Form.Group className={'email'}>
                     <Form.Label>EMAIL</Form.Label>
-                    <Form.Control type={'text'} placeholder={'mike-meta'}/>
+                    <Form.Control type={'text'} placeholder={'example@gmail.com'}/>
                 </Form.Group>
             </Form>
         </div>
