2baad46288391da8b320182e1f9bc4a83824a71b
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-09 19:36:58 +0300

    configured the redux provider

--- student-learning-assistant/src/main.jsx
+++ student-learning-assistant/src/main.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDom from 'react-dom/client'
 import App from "./App.jsx";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import store from "./AppPages/DataStore/store.js";
+import {Provider} from "react-redux";
+
 
 ReactDom.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 )
