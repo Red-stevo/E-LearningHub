91a1a378cb419337784ad80e811d5ec3ebce90ad
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-09 17:05:34 +0300

    input design is a go

--- student-learning-assistant/src/AppPages/Component/VerifyEmailForm.jsx
+++ student-learning-assistant/src/AppPages/Component/VerifyEmailForm.jsx
@@ -7,9 +7,11 @@ const VerifyEmailForm = () =>{
                 <legend><FormLabel>Verify Your Email</FormLabel></legend>
                 <Form.Text className={'text'}>Please check you email for a 6 digit verification code.</Form.Text>
 
-                <Form.Group>
-                    <Form.Label>CODE</Form.Label>
-                    <Form.Control maxLength={4} type="text"/>
+                <Form.Group className={'code-input'}>
+                    <Form.Label className={'label'}>CODE</Form.Label>
+                    <div className={'input-control'}>
+                        <Form.Control  maxLength={6} type="text"/>
+                    </div>
                 </Form.Group>
             </Form>
         </div>
