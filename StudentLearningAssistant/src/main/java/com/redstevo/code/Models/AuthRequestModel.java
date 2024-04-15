29e8db2c0d37411033fdd2829189a6a193922f67
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-15 10:45:58 +0300

    added some coments

--- StudentLearningAssistant/src/main/java/com/redstevo/code/Models/AuthRequestModel.java
+++ StudentLearningAssistant/src/main/java/com/redstevo/code/Models/AuthRequestModel.java
@@ -14,11 +14,15 @@ public class AuthRequestModel {
     private String username;
 
 
+    /*This will validate that the password entered during registration meets the condition in the regex*/
+
     @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^!`~*/.,<>?_;:&-+=()])(?=\\S+$).{8,20}$",
     message = "The password must contain at least 1 Upper case character, at least 1 lowercase character, a number " +
             "a special character and should have 8 - 20 characters")
     private  String password;
 
+    /*This ensures the email entered during registration follow the email pattern*/
     @Email(message = "You Entered An Invalid Email.")
+    @Size(max=100, message = "email is too long")
     private String email;
 }
