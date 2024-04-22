1f608a9dba0c3721148bac8d8bf3cb6581aab030
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-22 11:18:38 +0300

    Commit message >>>>> designed the page not found <<<<<

--- student-learning-assistant/src/AppPages/RoutedPages/NotFound.jsx
+++ student-learning-assistant/src/AppPages/RoutedPages/NotFound.jsx
@@ -1,11 +1,13 @@
 import {Image} from "react-bootstrap";
 import image from './../Images/pageNotFoundImage.png';
+import './../Styles/PageNotFoundStyles.css';
 
 export const NotFound = () =>{
     return(
-        <div className={'404'}>
-            <h1><b>404Page Not Found</b></h1>
+        <div className={'not-found'}>
+            <p><h1><b>404 Page Not Found</b></h1>
                 <Image src={image} height={300}/>
+            </p>
         </div>
     )
 }
