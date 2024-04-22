f16c265593b44ce63fe1aa9e9f9179548bf41a3a
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-22 15:58:26 +0300

    Commit message >>>>> restructured the registration page <<<<<

--- student-learning-assistant/src/AppPages/RoutedPages/RegistrationPage.jsx
+++ student-learning-assistant/src/AppPages/RoutedPages/RegistrationPage.jsx
@@ -1,29 +1,16 @@
 import {Container} from "react-bootstrap";
-import {useEffect, useState} from "react";
+import {useState} from "react";
 import Progress from "../Component/Progress.jsx";
 import RegistrationForm from "../Component/RegistrationForm.jsx";
 import VerifyEmailForm from "../Component/VerifyEmailForm.jsx";
 import './../Styles/RegistrationPage.css'
-import {useSelector} from "react-redux";
 
 
 export const RegistrationPage = () => {
-    const verifyBar = useSelector(state => state.RegStore.verifyBar);
-    const verifyDisplayState = useSelector(state => state.RegStore.verifyFormState);
-    const regDisplayState = useSelector(state => state.RegStore.regFormState);
-    const [register, setRegister] = useState(50);
+    const [register] = useState(50);
     const [verify, setVerify] = useState(0);
-    const [registrationPage, setRegistrationPage] = useState(regDisplayState);
-    const [verifyEmail, setVerifyEmail] = useState(verifyDisplayState);
-
-
-
-    useEffect(() => {
-        setVerify(verifyBar);
-        setRegistrationPage(regDisplayState);
-        setVerifyEmail(verifyDisplayState);
-
-    }, [verifyBar, verifyDisplayState, regDisplayState]);
+    const [registrationPage, setRegistrationPage] = useState(true);
+    const [verifyEmail, setVerifyEmail] = useState(false);
 
 
     return(
@@ -31,7 +18,8 @@ export const RegistrationPage = () => {
             <Progress register={register} verify={verify} />
             <hr />
             <div>
-                {registrationPage && <RegistrationForm />}
+                {registrationPage && <RegistrationForm
+                    register={setRegistrationPage} verify={setVerify} verificationCode={setVerifyEmail} />}
                 {verifyEmail && <VerifyEmailForm />}
             </div>
         </Container>
