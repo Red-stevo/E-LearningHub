42897000898db9e20215db9d34002930ffeb9fe0
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-22 17:15:38 +0300

    Commit message >>>>> caught a small token bug <<<<<

--- StudentLearningAssistant/src/main/java/com/redstevo/code/Services/AuthService.java
+++ StudentLearningAssistant/src/main/java/com/redstevo/code/Services/AuthService.java
@@ -344,7 +344,7 @@ public class AuthService {
 
         //generate the accessToken and set it to the client response.
         RefreshTokenModel refreshTokenModel = new RefreshTokenModel();
-        refreshTokenModel.setAccessToken(jwtService.generateToken(authTable));
+        refreshTokenModel.setAccessToken(generateToken(authTable));
 
         //user response.
         return ResponseEntity.ok(refreshTokenModel);
