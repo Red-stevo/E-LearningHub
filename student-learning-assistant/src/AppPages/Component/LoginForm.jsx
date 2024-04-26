77c0759cbe37cb029738123762a6ae978f0be882
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-26 17:28:40 +0300

    Commit message >>>>> styling the login page <<<<<

--- student-learning-assistant/src/AppPages/Component/LoginForm.jsx
+++ student-learning-assistant/src/AppPages/Component/LoginForm.jsx
@@ -1,21 +1,21 @@
 import {Button, Form, FormLabel} from "react-bootstrap";
-import './../Styles/RegistrationPage.css';
 import './../Styles/LoginPage.css';
 
 export const LoginForm = () =>{
     return(
         <Form className={"login-form"}>
-            <legend><FormLabel>LOGIN</FormLabel></legend>
-            <Form.Group>
+            <FormLabel>LOGIN</FormLabel>
+            <Form.Group className={"login-username"}>
                 <Form.Label htmlFor={"login-username"}>USERNAME </Form.Label>
                 <Form.Control id={"login-username"} type={"text"} required={true}/>
             </Form.Group>
 
-            <Form.Group>
+            <Form.Group className={"login-password"}>
                 <Form.Label htmlFor={"login-password"}>PASSWORD</Form.Label>
                 <Form.Control id={"login-password"} type={"password"} required={true}/>
             </Form.Group>
 
             <Button type={"submit"} className={'reg-btn'}>Login</Button>
+
         </Form>);
 }
--- student-learning-assistant/src/AppPages/RoutedPages/LoginPage.jsx
+++ student-learning-assistant/src/AppPages/RoutedPages/LoginPage.jsx
@@ -1,9 +1,12 @@
 import {Row} from "react-bootstrap";
 import {LoginForm} from "../Component/LoginForm.jsx";
+import './../Styles/RegistrationPage.css';
+import './../Styles/LoginPage.css';
+
 
 export const LoginPage = () => {
     return(
-        <Row>
+        <Row className={"login-page"}>
             <LoginForm />
         </Row>
     )
