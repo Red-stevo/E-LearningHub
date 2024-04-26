cc778e0062eecd41a30d3a132340baebe30bd87b
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-26 18:20:30 +0300

    Commit message >>>>> configured user input handling <<<<<

--- student-learning-assistant/src/AppPages/Component/LoginForm.jsx
+++ student-learning-assistant/src/AppPages/Component/LoginForm.jsx
@@ -1,18 +1,27 @@
 import {Button, Form, FormLabel} from "react-bootstrap";
 import './../Styles/LoginPage.css';
+import {useState} from "react";
 
 export const LoginForm = () =>{
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+
+
+
     return(
         <Form>
             <FormLabel>LOGIN</FormLabel>
             <Form.Group className={"login-username"}>
                 <Form.Label htmlFor={"login-username"}>USERNAME </Form.Label>
-                <Form.Control id={"login-username"} type={"text"} required={true}/>
+                <Form.Control id={"login-username"} type={"text"} required={true}
+                value={username} onChange={(e) => setUsername(e.target.value)}/>
             </Form.Group>
 
             <Form.Group className={"login-password"}>
                 <Form.Label htmlFor={"login-password"}>PASSWORD</Form.Label>
-                <Form.Control id={"login-password"} type={"password"} required={true}/>
+                <Form.Control id={"login-password"} type={"password"} required={true}
+                value={password} onChange={(e) => setPassword(e.target.value)}/>
             </Form.Group>
 
             <Button type={"submit"} className={'reg-btn'}>Login</Button>
