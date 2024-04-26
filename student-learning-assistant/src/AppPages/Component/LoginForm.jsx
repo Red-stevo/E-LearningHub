3162481fbb7274dbae9ab1e15222b72bc39adc29
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-26 17:05:55 +0300

    Commit message >>>>> styling the login page(basic) <<<<<

--- student-learning-assistant/src/AppPages/Component/LoginForm.jsx
+++ student-learning-assistant/src/AppPages/Component/LoginForm.jsx
@@ -1,9 +1,11 @@
-import {Button, Form} from "react-bootstrap";
-import './../Styles/RegistrationPage.css'
+import {Button, Form, FormLabel} from "react-bootstrap";
+import './../Styles/RegistrationPage.css';
+import './../Styles/LoginPage.css';
 
 export const LoginForm = () =>{
     return(
-        <Form>
+        <Form className={"login-form"}>
+            <legend><FormLabel>LOGIN</FormLabel></legend>
             <Form.Group>
                 <Form.Label htmlFor={"login-username"}>USERNAME </Form.Label>
                 <Form.Control id={"login-username"} type={"text"} required={true}/>
