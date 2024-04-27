2928ccf76e7df74e0a80594492aa9749f3718b46
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-04-27 05:55:30 +0300

    Commit message >>>>> optimized the code <<<<<

--- StudentLearningAssistant/src/main/java/com/redstevo/code/Services/JwtService.java
+++ StudentLearningAssistant/src/main/java/com/redstevo/code/Services/JwtService.java
@@ -17,7 +17,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
-import java.nio.file.LinkOption;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
