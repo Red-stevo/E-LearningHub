d9917f6384aa98098fabca7cc8ee32270dc08798
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-05-04 17:26:59 +0300

    Commit message >>>>> navbar design <<<<<

--- student-learning-assistant/src/AppPages/Component/MainHeader.jsx
+++ student-learning-assistant/src/AppPages/Component/MainHeader.jsx
@@ -4,18 +4,22 @@ import {Outlet} from "react-router";
 export const MainHeader = () => {
     return (<>
         <div className={"main-header"}>
-            <div className={"nav-title"}>Learning Assistant</div>
-            <Navbar expand="sm">
-                <Navbar.Toggle aria-controls="navbarScroll"/>
-                <Navbar.Collapse id="navbarScroll">
-                    <Nav navbarScroll className={"main-navbar"}>
-                        <Nav.Link href={"/student-assistant/learn/main"}>Home</Nav.Link>
-                        <Nav.Link href={"/student-assistant/learn/add/course"}> Create Course</Nav.Link>
-                        <Nav.Link href={"/student-assistant/learn/statistics"}>Learning Statistics</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-            <div className={"mx-5 mx-md-3 mx-sm-3"} href={"/student-assistant/learn/main"}>User Profile</div>
+            <div className={"main-title"}>Learning Assistant</div>
+
+            <div className={"nav-holder"}>
+                <Navbar expand="sm">
+                    <Navbar.Toggle aria-controls="navbarScroll"/>
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav navbarScroll className={"main-navbar"}>
+                            <Nav.Link href={"/student-assistant/learn/main"}>Home</Nav.Link>
+                            <Nav.Link href={"/student-assistant/learn/add/course"}> Create Course</Nav.Link>
+                            <Nav.Link href={"/student-assistant/learn/statistics"}>Learning Statistics</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
+
+            <div className={"profile"} >User Profile</div>
         </div>
         <div>
             <Outlet/>
