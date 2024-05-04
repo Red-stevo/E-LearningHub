4e9555dcef96402302a378fcfc5f14bccf3ccd45
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-05-04 16:58:02 +0300

    Commit message >>>>> checking on the responsiveness of the main header <<<<<

--- student-learning-assistant/src/AppPages/Component/MainHeader.jsx
+++ student-learning-assistant/src/AppPages/Component/MainHeader.jsx
@@ -1,21 +1,24 @@
-import {Nav, Navbar, NavbarBrand} from "react-bootstrap";
+import {Nav, Navbar} from "react-bootstrap";
 import './../Styles/mainPage.css'
 import {Outlet} from "react-router";
 export const MainHeader = () => {
-    return(<>
-        <Navbar expand="sm" className={"main-header"}>
-            <NavbarBrand>Learning Assistant</NavbarBrand>
-            <Navbar.Toggle aria-controls="navbarScroll"/>
-            <Navbar.Collapse id="navbarScroll" >
-                <Nav navbarScroll className={"main-navbar"}>
-                    <Nav.Link href={"/student-assistant/learn/main"}>Home</Nav.Link>
-                    <Nav.Link href={"/student-assistant/learn/add/course"}> Create Course</Nav.Link>
-                    <Nav.Link href={"/student-assistant/learn/statistics"} >Learning Statistics</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
-            <Nav.Link className={"mx-5"} href={"/student-assistant/learn/main"} >User Profile</Nav.Link>
-        </Navbar>
-
-        <Outlet />
-        </>);
+    return (<>
+        <div className={"main-header"}>
+            <div className={"nav-title"}>Learning Assistant</div>
+            <Navbar expand="sm">
+                <Navbar.Toggle aria-controls="navbarScroll"/>
+                <Navbar.Collapse id="navbarScroll">
+                    <Nav navbarScroll className={"main-navbar"}>
+                        <Nav.Link href={"/student-assistant/learn/main"}>Home</Nav.Link>
+                        <Nav.Link href={"/student-assistant/learn/add/course"}> Create Course</Nav.Link>
+                        <Nav.Link href={"/student-assistant/learn/statistics"}>Learning Statistics</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            <div className={"mx-5 mx-md-3 mx-sm-3"} href={"/student-assistant/learn/main"}>User Profile</div>
+        </div>
+        <div>
+            <Outlet/>
+        </div>
+    </>);
 }
