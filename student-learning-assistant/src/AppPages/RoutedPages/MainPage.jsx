c955a60d0b1b33ed1db6c38169ff6d0bd30d3c68
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-05-04 07:12:45 +0300

    Commit message >>>>> designing the main navigation bar <<<<<

--- student-learning-assistant/src/AppPages/RoutedPages/MainPage.jsx
+++ student-learning-assistant/src/AppPages/RoutedPages/MainPage.jsx
@@ -1,19 +1,18 @@
-import {Nav, Navbar} from "react-bootstrap";
-
+import {Nav, Navbar, NavbarBrand} from "react-bootstrap";
+import './../Styles/mainPage.css'
 
 export const MainPage = () => {
 
     return (
-        <div>
-            <Navbar expand="sm">
-                    <Navbar.Toggle aria-controls="navbarScroll"/>
-                    <Navbar.Collapse id="navbarScroll" >
-                        <Nav navbarScroll>
-                            <Nav.Link href={"/student-assistant/main"}>Home</Nav.Link>
-                            <Nav.Link href={"/student-assistant/main"}>Collection</Nav.Link>
-                            <Nav.Link href={"/student-assistant/main"}>User Profile</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-            </Navbar>
-        </div>);
+            <Navbar expand="sm" className={"main-header"}>
+                <NavbarBrand>Learning Assistant</NavbarBrand>
+                <Navbar.Toggle aria-controls="navbarScroll"/>
+                <Navbar.Collapse id="navbarScroll" >
+                    <Nav navbarScroll className={"main-navbar"}>
+                        <Nav.Link href={"/student-assistant/main"}>Home</Nav.Link>
+                        <Nav.Link href={"/student-assistant/main"}>Collection</Nav.Link>
+                        <Nav.Link href={"/student-assistant/main"}>User Profile</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>);
 }
