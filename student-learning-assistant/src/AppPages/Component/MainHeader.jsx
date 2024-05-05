037f048a02741afc92592e372776d18c7de173fd
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-05-05 18:35:16 +0300

    Commit message >>>>> taking a break <<<<<

--- student-learning-assistant/src/AppPages/Component/MainHeader.jsx
+++ student-learning-assistant/src/AppPages/Component/MainHeader.jsx
@@ -3,11 +3,26 @@ import './../Styles/mainPage.css'
 import {Outlet} from "react-router";
 import "./../Styles/mainPage.css"
 import {ProfileImage} from "./ProfileImage.jsx";
+import {useEffect, useState} from "react";
+
 export const MainHeader = () => {
+    const [mainHeader, setMainHeader] = useState([]);
+    const [counter, setCounter] = useState(0);
+    const header = ['L', 'e', 'a', 'r', 'n', 'i', 'n', 'g', ' ', 'A', 's', 's', 'i', 's', 't', 'a', 'n', 't'];
+
+    useEffect(() => {
+
+        setInterval(() => {
+            setCounter(counter + 1);
+            console.log(counter);
+        }, 2000)
+    }, []);
+
+
     return (<>
         <div className={"main-header"}>
 
-            <div className={"main-title"}>Learning Assistant</div>
+            <div className={"main-title"}>{mainHeader}</div>
 
             <div className={"nav-profile"}>
 
