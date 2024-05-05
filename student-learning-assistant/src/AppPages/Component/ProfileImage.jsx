a901ef5a6748e12013c6d82d5e403e82d2e3fc4e
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-05-05 12:07:43 +0300

    Commit message >>>>> designed the image profile icon <<<<<

--- student-learning-assistant/src/AppPages/Component/ProfileImage.jsx
+++ student-learning-assistant/src/AppPages/Component/ProfileImage.jsx
@@ -4,8 +4,8 @@ import image from "../Images/learn2.jpeg";
 export const ProfileImage = () => {
     return(<>
         <div className={"profile-component"}>
-            <div className={"profile-image"} >
-                <img src={image} height={"100%"} width={"100%"} alt={"user profile image"}/>
+            <div className={"profile-image"}>
+                <img src={image} height={"100%"} width={"100%"} alt={"user profile image"} className={"image"}/>
             </div>
             <span className={"profile-title"}>Profile</span>
         </div>
