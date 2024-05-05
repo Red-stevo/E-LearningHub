a7f044360c9115830ab998fdbb25e843d4dc32ed
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-05-05 12:53:22 +0300

    Commit message >>>>> styling the page <<<<<

--- student-learning-assistant/src/AppPages/Component/MainHeader.jsx
+++ student-learning-assistant/src/AppPages/Component/MainHeader.jsx
@@ -2,13 +2,13 @@ import {Nav, Navbar} from "react-bootstrap";
 import './../Styles/mainPage.css'
 import {Outlet} from "react-router";
 import "./../Styles/mainPage.css"
-import image from './../Images/logo.svg'
 import {ProfileImage} from "./ProfileImage.jsx";
 export const MainHeader = () => {
     return (<>
         <div className={"main-header"}>
 
             <div className={"main-title"}>Learning Assistant</div>
+            <div className={"nav-profile"}>
 
             <div className={"nav-holder"}>
                 <Navbar expand="sm">
@@ -16,13 +16,15 @@ export const MainHeader = () => {
                     <Navbar.Collapse id="navbarScroll">
                         <Nav navbarScroll className={"main-navbar"}>
                             <Nav.Link href={"/student-assistant/learn/main"}>Home</Nav.Link>
-                            <Nav.Link href={"/student-assistant/learn/add/course"}> Create Course</Nav.Link>
-                            <Nav.Link href={"/student-assistant/learn/statistics"}>Learning Statistics</Nav.Link>
+                            <Nav.Link href={"/student-assistant/learn/add/course"}> Course</Nav.Link>
+                            <Nav.Link href={"/student-assistant/learn/statistics"}> Statistics</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Navbar>
             </div>
-            <ProfileImage />
+
+                <ProfileImage />
+            </div>
         </div>
         <div>
             <Outlet/>
