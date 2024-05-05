a240f8630ba2e5e1e316c1545695cf901e4582c8
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-05-05 12:01:09 +0300

    Commit message >>>>> configured the image placement loction <<<<<

--- student-learning-assistant/src/AppPages/Component/ProfileImage.jsx
+++ student-learning-assistant/src/AppPages/Component/ProfileImage.jsx
@@ -1,11 +1,11 @@
-import image from "../Images/logo.svg";
+import image from "../Images/learn2.jpeg";
 
 
 export const ProfileImage = () => {
     return(<>
         <div className={"profile-component"}>
-            <div className={"profile-image"}>
-                {/*<img src={image} height={"90%"} width={"30%"} alt={"user profile image"}/>*/}
+            <div className={"profile-image"} >
+                <img src={image} height={"100%"} width={"100%"} alt={"user profile image"}/>
             </div>
             <span className={"profile-title"}>Profile</span>
         </div>
