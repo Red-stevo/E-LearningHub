85651ee0ad0f4da503270e4012dd0abb53067248
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-05-07 06:46:07 +0300

    Commit message >>>>> main page design is a go <<<<<

--- student-learning-assistant/src/AppPages/Component/MainHeader.jsx
+++ student-learning-assistant/src/AppPages/Component/MainHeader.jsx
@@ -3,45 +3,34 @@ import './../Styles/mainPage.css'
 import {Outlet} from "react-router";
 import "./../Styles/mainPage.css"
 import {ProfileImage} from "./ProfileImage.jsx";
-import {useEffect, useState} from "react";
+import {ReactTyped} from "react-typed";
+
 
 export const MainHeader = () => {
-    const [mainHeader, setMainHeader] = useState([]);
-    const [counter, setCounter] = useState(0);
-    const header = ['L', 'e', 'a', 'r', 'n', 'i', 'n', 'g', ' ', 'A', 's', 's', 'i', 's', 't', 'a', 'n', 't'];
 
-    useEffect(() => {
-
-        setInterval(() => {
-            setCounter(counter + 1);
-            console.log(counter);
-        }, 2000)
-    }, []);
 
 
     return (<>
         <div className={"main-header"}>
-
-            <div className={"main-title"}>{mainHeader}</div>
-
+            <div className={"main-title"}>
+                <ReactTyped strings={["Learning Assistant"]} typeSpeed={100} backSpeed={80} backDelay={50}
+                            startDelay={80} loop={true} showCursor={true}/>
+            </div>
             <div className={"nav-profile"}>
-
-            <div className={"nav-holder"}>
-                <Navbar expand="sm">
-                    <Navbar.Toggle aria-controls="navbarScroll"/>
-                    <Navbar.Collapse id="navbarScroll">
-                        <Nav navbarScroll className={"main-navbar"}>
-                            <Nav.Link href={"/student-assistant/learn/main"}>Home</Nav.Link>
-                            <Nav.Link href={"/student-assistant/learn/add/course"}> Course</Nav.Link>
-                            <Nav.Link href={"/student-assistant/learn/statistics"}> Statistics</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Navbar>
-            </div>
-
-                <ProfileImage />
-
-            </div>
+                <div className={"nav-holder"}>
+                    <Navbar expand="sm">
+                        <Navbar.Toggle aria-controls="navbarScroll"/>
+                        <Navbar.Collapse id="navbarScroll">
+                            <Nav navbarScroll className={"main-navbar"}>
+                                <Nav.Link href={"/student-assistant/learn/main"}>Home</Nav.Link>
+                                <Nav.Link href={"/student-assistant/learn/add/course"}> Course</Nav.Link>
+                                <Nav.Link href={"/student-assistant/learn/statistics"}> Statistics</Nav.Link>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Navbar>
+                </div>
+                    <ProfileImage />
+                </div>
         </div>
         <div>
             <Outlet/>
