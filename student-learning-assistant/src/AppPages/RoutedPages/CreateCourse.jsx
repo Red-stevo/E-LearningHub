ce33e9ee73c9873b10a10a9729333781cb266216
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-05-07 08:05:43 +0300

    Commit message >>>>> taking a break <<<<<

--- student-learning-assistant/src/AppPages/RoutedPages/CreateCourse.jsx
+++ student-learning-assistant/src/AppPages/RoutedPages/CreateCourse.jsx
@@ -4,6 +4,16 @@ export const CreateCourse = () => {
     return(<div>
         <Form>
             <FormLabel className={"form-title"}>Create Topic Collection</FormLabel>
+
+            <Form.Group>
+                <Form.Label htmlFor={"course"}>Course Name </Form.Label>
+                <Form.Control id={"course"} type={"text"}/>
+            </Form.Group>
+
+            <Form.Group>
+                <Form.Label htmlFor={"description-file"}>Description File</Form.Label>
+                <Form.Control id={"description-file"} type={"checked"} />
+            </Form.Group>
         </Form>
     </div>)
 }
