2bbe48b7340648f9de37767a46a969923b49b8df
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-05-08 08:27:23 +0300

    Commit message >>>>> added the collection creation page <<<<<

--- student-learning-assistant/src/AppPages/RoutedPages/CreateCourse.jsx
+++ student-learning-assistant/src/AppPages/RoutedPages/CreateCourse.jsx
@@ -1,18 +1,18 @@
 import {Form, FormLabel} from "react-bootstrap";
 import './../Styles/CreateCourse.css'
 export const CreateCourse = () => {
-    return(<div>
-        <Form>
+    return(<div className={"collection-form"}>
+        <Form className={"create-collection"}>
             <FormLabel className={"form-title"}>Create Topic Collection</FormLabel>
 
-            <Form.Group>
+            <Form.Group className={"collection-name"}>
                 <Form.Label htmlFor={"course"}>Course Name </Form.Label>
                 <Form.Control id={"course"} type={"text"}/>
             </Form.Group>
 
             <Form.Group>
-                <Form.Label htmlFor={"description-file"}>Description File</Form.Label>
-                <Form.Control id={"description-file"} type={"checkbox"} />
+                <Form.Label htmlFor={"description-file"} className={"description-label"}>Add Description File</Form.Label>
+                 <input id={"description-file"} type={"checkbox"}/>
             </Form.Group>
         </Form>
     </div>)
