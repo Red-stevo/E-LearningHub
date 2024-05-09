c8d4b28cb3045e6cf19af0dee009b6418009ed09
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-05-09 07:01:49 +0300

    Commit message >>>>> short endpoint comment <<<<<

--- StudentLearningAssistant/src/main/java/com/redstevo/code/Controllers/LearningController.java
+++ StudentLearningAssistant/src/main/java/com/redstevo/code/Controllers/LearningController.java
@@ -13,11 +13,16 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/v1/learn")
 public class LearningController {
 
+    /*
+    * This end point will :-
+    *   1. Create a new Collection.
+    *   2. If User allows a new Description file will be created to describe the course.
+    *   3. Relate the Collection to a user and the Description file to the collection.
+    */
+
     @PostMapping("/new/collection")
     public ResponseEntity<Model> createCollection(
             @RequestParam String CollectionName, @RequestParam Boolean CreateDescFile){
-
-
 
         return null;
     }
