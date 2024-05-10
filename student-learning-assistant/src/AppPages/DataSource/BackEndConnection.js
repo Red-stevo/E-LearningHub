7153e4fd7780b424f5772fb578099aaa5a497ceb
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-05-10 05:46:54 +0300

    Commit message >>>>> added request comments <<<<<

--- student-learning-assistant/src/AppPages/DataSource/BackEndConnection.js
+++ student-learning-assistant/src/AppPages/DataSource/BackEndConnection.js
@@ -11,25 +11,39 @@ export async function registerUser(userData){
     return await (guestAPIs.post("/register",userData));
 }
 
+
+/*End point to check whether the username is already in use.*/
 export async function userNameCheck(username){
     return await (guestAPIs.get(`/check/username?username=${username}`));
 }
+
+
+/*Endpoint to check whether the email is already in use.*/
 export async function emailCheckAvailable(email){
     return await (guestAPIs.get(`/check/email?email=${email}`));
 }
 
+
+/*End point to verify the email confirmation code sent*/
 export async function verifyEmailCode(code, username){
     return await (guestAPIs.put(`/verify/code?code=${code}&username=${username}`));
 }
 
+
+/*End point to resend an email if it fails to go through*/
 export async function resendVerificationCode(username){
     return await (guestAPIs.put(`/verify/resend?username=${username}`));
 }
 
+
+
+/*Login end point*/
 export async function userLogin(loginModel){
     return await (guestAPIs.post("/login", loginModel));
 }
 
+
+/*End Point Get a new Access and refresh token*/
 export async function refreshToken(){
     return await (guestAPIs.put("/token/refresh"))
 }
