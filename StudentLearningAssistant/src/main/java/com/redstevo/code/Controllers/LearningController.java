b84951fe9fd386c530bf9ec2563bb77686571220
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-05-10 06:04:43 +0300

    Commit message >>>>> @CrossOrigin() <<<<<

--- StudentLearningAssistant/src/main/java/com/redstevo/code/Controllers/LearningController.java
+++ StudentLearningAssistant/src/main/java/com/redstevo/code/Controllers/LearningController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/learn")
+@CrossOrigin(value = "http://localhost:5173", allowCredentials = "true")
 public class LearningController {
 
     private final LearningService learningService;
