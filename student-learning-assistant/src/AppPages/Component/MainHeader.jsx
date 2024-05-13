59fef374a1792ccd8b631832f4e1b07270db380b
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-05-13 09:52:47 +0300

    Commit message >>>>> styling the headinf text <<<<<

--- student-learning-assistant/src/AppPages/Component/MainHeader.jsx
+++ student-learning-assistant/src/AppPages/Component/MainHeader.jsx
@@ -12,12 +12,12 @@ export const MainHeader = () => {
 
     return (<>
         <div className={"main-header"}>
-            <div className={"main-title"}>
-                <ReactTyped strings={["Learning Assistant"]} typeSpeed={200} backSpeed={200} backDelay={100}
+            <div id={"text-header"}  className={"main-title"}>
+                <ReactTyped  strings={["Learning Assistant"]}
+                            typeSpeed={200} backSpeed={200} backDelay={100}
                             startDelay={80} loop={true} showCursor={true}
                             style={{fontFamily: '"Jacquard 24 Charted", system-ui', fontWeight: 400, fontStyle: 'normal',
-                            fontSize:"40px"}}
-                            id={"text-header"} />
+                            fontSize:"30px"}}/>
             </div>
             <div className={"nav-profile"}>
                 <div className={"nav-holder"}>
