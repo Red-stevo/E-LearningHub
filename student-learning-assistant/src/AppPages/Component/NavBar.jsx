9fb9ec6dda1fb54947e2294df54b3a09cf036c39
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-06-07 11:42:25 +0300

    Commit message >>>>> designing the home page header. <<<<<

--- student-learning-assistant/src/AppPages/Component/NavBar.jsx
+++ student-learning-assistant/src/AppPages/Component/NavBar.jsx
@@ -1,11 +1,14 @@
-import {Navbar, NavbarBrand, NavLink} from "react-bootstrap";
+import {Image, Nav, Navbar, NavbarBrand, NavLink} from "react-bootstrap";
+import logo from "../Images/logo.jpg"
 
 const NavBar = () => {
     return(<>
-        <Navbar expand={"md"}>
-            <NavbarBrand></NavbarBrand>
-            <NavLink>Login</NavLink>
-            <NavLink>Create Account</NavLink>
+        <Navbar expand={"md"} className={"home-nav"}>
+            <NavbarBrand><Image src={logo}  height={90} width={130} id={"logo"}/></NavbarBrand>
+            <Nav>
+                <NavLink>Login</NavLink>
+                <NavLink>Create Account</NavLink>
+            </Nav>
         </Navbar>
     </>);
 }
