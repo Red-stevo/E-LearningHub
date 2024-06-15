d7560a1c4a4c127ef445de74a1d1eb359467e317
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-06-15 17:58:26 +0300

    Commit message >>>>> configuring the progress bar data flow <<<<<

--- student-learning-assistant/src/AppPages/Component/Progress.jsx
+++ student-learning-assistant/src/AppPages/Component/Progress.jsx
@@ -1,15 +1,19 @@
 import {ProgressBar} from "react-bootstrap";
 import './../Styles/RegistrationPage.css'
-// eslint-disable-next-line react/prop-types
-const Progress = ( { register, verify} ) => {
+import {useContext} from "react";
+import {ProgressBarUpdates} from "../DataStore/ProgressBarUpdates.js";
+const Progress = () => {
+    const {regBar, verBar} = useContext(ProgressBarUpdates);
+
+
     return(
         <div className={"bar"}>
             <ProgressBar className={'bg-dark'} >
                 <ProgressBar
-                    striped={false} now={register} className={'register '} variant={'danger'}
+                    striped={false} now={regBar} className={'register '} variant={'danger'}
                     label={"REGISTER"}/>
                 <ProgressBar
-                    striped={true} now={verify} className={'verify-email'} variant={'success'}
+                    striped={true} now={verBar} className={'verify-email'} variant={'success'}
                     label={"VERIFY EMAIL"}/>
             </ProgressBar>
         </div>
--- student-learning-assistant/src/AppPages/RoutedPages/RegistrationPage.jsx
+++ student-learning-assistant/src/AppPages/RoutedPages/RegistrationPage.jsx
@@ -1,16 +1,20 @@
 import Progress from "../Component/Progress.jsx";
 import './../Styles/RegistrationPage.css'
 import {Outlet} from "react-router";
+import {ProgressBarUpdates} from "../DataStore/ProgressBarUpdates.js";
+import {useState} from "react";
 
 
 export const RegistrationPage = () => {
+   const [bar, setBar] = useState({regBar:0, verBar:0});
+
     return(
-        <>
+        <ProgressBarUpdates.Provider value={{bar, setBar}}>
             <div className={"page1"}>
-                <Progress/>
+                <Progress />
             </div>
                 <Outlet />
-        </>
+        </ProgressBarUpdates.Provider>
     );
 }
 
