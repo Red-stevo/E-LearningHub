c5db3b08424e3cecca74193068e9200dcea6aa87
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-06-16 15:10:04 +0300

    Commit message >>>>> reduced the code used <<<<<

--- student-learning-assistant/src/AppPages/Component/HomePageSectionA.jsx
+++ student-learning-assistant/src/AppPages/Component/HomePageSectionA.jsx
@@ -3,19 +3,17 @@ import mainText from "./../Images/homeTitle.svg"
 import {Image} from "react-bootstrap";
 import HomeDesign from "./HomeDesign.jsx";
 import {useEffect} from "react";
-import useScreenSize from "../../CustomeHooks/useScreenSize.js";
+
 
 const HomePageSectionA = () => {
-    const size = useScreenSize();
+
+    useEffect(() => {
+        window.addEventListener("screenResize", updateHomePage);
+    }, []);
+
     const updateHomePage = () => {
 
     }
-
-    useEffect(() => {
-        if(size === 810){
-            updateHomePage();
-        }
-    }, [size]);
 
 
     return(<div className={"home-page"}>
